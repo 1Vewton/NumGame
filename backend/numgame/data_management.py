675e2cfa0f04e33481aca865326e295433e2db9a
NewtonZhan <zhanyunze0601@gmail.com>
2026-02-19 22:30:51 +0800

    feat(data models): Adding data models for games and players

--- backend/numgame/data_management.py
+++ backend/numgame/data_management.py
@@ -1,3 +1,7 @@
 from numgame.config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 # Database Connection
+engine = create_engine(settings.database_url)
+DBSession = sessionmaker(bind=engine)
---
+++ backend/numgame/data_models.py
@@ -0,0 +1,37 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime, Integer, ForeignKey, CheckConstraint
+from sqlalchemy.orm import relationship
+
+# Base
+Base = declarative_base()
+
+# Games data
+class Games(Base):
+    # Table name
+    __tablename__ = 'Games'
+    # Columns
+    id = Column(String(100), primary_key=True)
+    first_move = Column(String(100), ForeignKey("Players.id"),nullable=False)
+    second_move = Column(String(100), ForeignKey("Players.id"), nullable=False)
+    winner = Column(String(100), ForeignKey("Players.id"), nullable=True)
+    rounds = Column(Integer, nullable=False)
+    started_time = Column(DateTime, nullable=False)
+    # constraints
+    __table_args__ = (
+        CheckConstraint('first_move != second_move', name='check_players_different'),
+    )
+    # Relations
+    first_player = relationship("Players", foreign_keys=[first_move])
+    second_player = relationship("Players", foreign_keys=[second_move])
+    winner_player = relationship("Players", foreign_keys=[winner])
+
+# Player data
+class Players(Base):
+    # Table name
+    __tablename__ = "Players"
+    # Columns
+    id = Column(String(100), primary_key=True)
+    user_name = Column(String(100), unique=True, nullable=False)
+    registered_at = Column(DateTime, nullable=False)
+    wins = Column(Integer, nullable=False)
+    total_games = Column(Integer, nullable=False)
