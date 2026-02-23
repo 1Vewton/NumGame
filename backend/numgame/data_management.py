8a5bfa681b52aadf7f61aae41d17655e9ddf6546
NewtonZhan <zhanyunze0601@gmail.com>
2026-02-23 10:48:57 +0800

    feat(app): Adding lifespan for the app

--- backend/numgame/data_management.py
+++ backend/numgame/data_management.py
@@ -3,7 +3,9 @@ from numgame.data_models import Base
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from collections.abc import AsyncGenerator
 from typing import Any
+import logging
 
+logger = logging.getLogger("DataBaseManager")
 # DB connection
 async_engine = create_async_engine(settings.database_url, echo=False)
 DBSession = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
@@ -15,5 +17,4 @@ async def get_db() -> AsyncGenerator[AsyncSession, Any]:
 async def init_models() -> None:
     async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-# Table Detection
-
+        logger.info("Database tables initialized.")
