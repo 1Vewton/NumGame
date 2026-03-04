01b78f18d019a44ded58a996d2dc536dd3382bbb
NewtonZhan <zhanyunze0601@gmail.com>
2026-03-04 09:24:04 +0800

    feat(game): Adding game operations enum

---
+++ backend/numgame/enums.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+# Game operations
+class Operations(Enum):
+    PRODUCE = 1
+    DESTRUCT = 2
+    ENHANCE_PRODUCTIVITY = 3
+    ENHANCE_DESTRUCTIBILITY = 4
+    ENHANCE_ACTION_POINT = 5
