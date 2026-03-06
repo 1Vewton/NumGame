d87af3c3acf4701d9832b3ccd7e93fb608a60a5a
NewtonZhan <zhanyunze0601@gmail.com>
2026-03-06 09:32:37 +0800

    feat(bot): Creating bot when the server is started

--- backend/numgame/server.py
+++ backend/numgame/server.py
@@ -1,22 +1,49 @@
 from logging import getLogger
 import uvicorn
+import asyncio
 from collections.abc import AsyncGenerator
-from typing import Any
+from typing import Any, Annotated
 from contextlib import asynccontextmanager
 # FastAPI dependencies
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 # Project Dependencies
 from numgame.config import settings
-from numgame.data_management import init_models
+from numgame.data_management import init_models, get_db
+from numgame.data_models import players
 import numgame.logger_config as logger_config
 from numgame.redis_manager import create_redis_client
 from numgame.server_user_management import user_router
 from numgame.server_utils import utils_router
-from numgame.utils import limiter
+from numgame.utils import limiter, generate_uuid
 # Slowapi Dependencies
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
+# ORM Dependencies
+from sqlalchemy import select
+from datetime import datetime
 
+# Create bot
+async def create_bot_account():
+    async for session in get_db():
+        # Find simple bot
+        existing = await session.execute(select(players).where(players.user_name == settings.simple_bot_name))
+        if existing.scalar_one_or_none():
+            # Bot already exists
+            logger.info("Bot exists")
+        else:
+            logger.info("Creating new bot")
+            # Bot creation
+            bot_id = generate_uuid()
+            bot_name = settings.simple_bot_name
+            registration_date = datetime.now()
+            is_bot = True
+            bot = players(
+                id=bot_id,
+                user_name=bot_name,
+                registered_at=registration_date,
+                is_bot=is_bot
+            )
+            session.add(bot)
 # run before execution
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[Any, None]:
@@ -25,6 +52,7 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[Any, None]:
     # Redis Config
     redis_client = await create_redis_client()
     app.state.redis_client = redis_client
+    asyncio.create_task(create_bot_account())
     yield
     await redis_client.aclose()
     logger.info(f"Redis connection closed")
