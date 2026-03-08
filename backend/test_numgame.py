2f8f0360117e84e39dbbbbdf17c9559b5ba0d13c
NewtonZhan <zhanyunze0601@gmail.com>
2026-03-08 08:13:17 +0800

    feat(backend): Add testing

---
+++ backend/test_numgame.py
@@ -0,0 +1,30 @@
+# Project dependencies
+from numgame.server import app
+from numgame.redis_manager import create_redis_client
+# Test dependencies
+from fastapi.testclient import TestClient
+import pytest
+import fakeredis
+
+# Overwrite Redis
+# Create fake redis client
+@pytest.fixture
+def fake_redis():
+    server = fakeredis.FakeServer()
+    client = fakeredis.FakeRedis(server=server, decode_responses=True)
+    return client
+# Auto change the redis to fakeredis
+@pytest.fixture(autouse=True)
+def mock_redis_client(monkeypatch, fake_redis):
+    async def mock_create_redis_client():
+        return fake_redis
+    monkeypatch.setattr("numgame.redis_manager.create_redis_client", mock_create_redis_client)
+# test client
+@pytest.fixture
+def client():
+    with TestClient(app) as test_client:
+        yield test_client
+
+def test_random_name(client):
+    response = client.get("/api/utils/generateUserName")
+    assert response.status_code == 200
