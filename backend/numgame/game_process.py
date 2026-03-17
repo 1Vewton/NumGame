54bb22a1eeaf68c65651c20734149913d1fdf252
NewtonZhan <zhanyunze0601@gmail.com>
2026-03-17 08:01:24 +0800

    feat(game): Adding the functions for getting the data

--- backend/numgame/game_process.py
+++ backend/numgame/game_process.py
@@ -34,6 +34,10 @@ class GameProcess:
         # Store it to hash table
         await self.client.hset(self.game_id, mapping=score_info)
 
+    '''
+    Action point
+    '''
+
     # Get bot action point
     async def getBotActionPoint(self):
         logger.info("Trying to get the action point for bot")
@@ -44,7 +48,7 @@ class GameProcess:
         return bot_action_point
 
     # Get player action point
-    async def playerActionPoint(self):
+    async def getPlayerActionPoint(self):
         logger.info("Trying to get the action point for player")
         player_action_point = await self.client.hget(
             self.game_id,
@@ -52,8 +56,30 @@ class GameProcess:
         )
         return player_action_point
 
+    # Get bot action point per turn
+    async def getBotActionPointPT(self):
+        logger.info("Trying to get the action point per turn for bot")
+        bot_action_point_per_turn = await self.client.hget(
+            self.game_id,
+            "bot_action_point_per_turn"
+        )
+        return bot_action_point_per_turn
+
+    # Get player action point per turn
+    async def getPlayerActionPointPT(self):
+        logger.info("Trying to get the action point per turn for player")
+        player_action_point_per_turn = await self.client.hget(
+            self.game_id,
+            "player_action_point_per_turn"
+        )
+        return player_action_point_per_turn
+
+    '''
+    Score
+    '''
+
     # Get player score
-    async def playerScore(self):
+    async def getPlayerScore(self):
         logger.info("Trying to get the score for player")
         player_score = await self.client.hget(
             self.game_id,
@@ -62,7 +88,7 @@ class GameProcess:
         return player_score
 
     # Get bot score
-    async def botScore(self):
+    async def getBotScore(self):
         logger.info("Trying to get the score for bot")
         bot_score = await self.client.hget(
             self.game_id,
@@ -70,14 +96,49 @@ class GameProcess:
         )
         return bot_score
 
-    # Get bot action point per turn
-    async def botActionPointPT(self):
-        logger.info("Trying to get the action point per turn for bot")
-        bot_action_point_per_turn = await self.client.hget(
+    '''
+    Productivity
+    '''
+
+    # Get player productivity
+    async def getPlayerProductivity(self):
+        logger.info("Trying to get the productivity for player")
+        player_productivity = await self.client.hget(
             self.game_id,
-            "bot_action_point_per_turn"
+            "player_productivity"
         )
-        return bot_action_point_per_turn
+        return player_productivity
+
+    # Get bot productivity
+    async def getBotProductivity(self):
+        logger.info("Trying to get the productivity for bot")
+        bot_productivity = await self.client.hget(
+            self.game_id,
+            "bot_productivity"
+        )
+        return bot_productivity
+
+    '''
+    Destructivity
+    '''
+
+    # Get the destructivity of player
+    async def getPlayerDestructivity(self):
+        logger.info("Trying to get the destructivity for player")
+        player_destructivity = await self.client.hget(
+            self.game_id,
+            "player_destructivity"
+        )
+        return player_destructivity
+
+    # Get the destructivity of bot
+    async def getBotDestructivity(self):
+        logger.info("Trying to get the destructivity for bot")
+        bot_destructivity = await self.client.hget(
+            self.game_id,
+            "bot_destructivity"
+        )
+        return bot_destructivity
 
     # Delete game data
     async def deleteData(self):
