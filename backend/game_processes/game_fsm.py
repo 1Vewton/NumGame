e65c4296cc21bf4772c099b0d37072092effc316
NewtonZhan <zhanyunze0601@gmail.com>
2026-04-01 08:42:02 +0800

    feat(game): Add initialization of game

--- backend/game_processes/game_fsm.py
+++ backend/game_processes/game_fsm.py
@@ -7,7 +7,11 @@ from fastapi import WebSocket
 # project dependencies
 from data_management.enums import GameState
 from game_processes.bot_game_process import BotGameProcess
-from utils.utils import generate_uuid
+from utils.utils import (
+    generate_uuid,
+    decide_is_user_first
+)
+from data_management.enums import WSResponseType
 
 logger = getLogger("game_fsm")
 
@@ -18,7 +22,7 @@ class GameStateMachine:
                  client: WebSocket,
                  redis_client: aioredis.Redis,
                  ):
-        self.client = client
+        self.ws_client = client
         # async lock
         self.lock = asyncio.Lock()
         self.state = GameState.INIT
@@ -26,8 +30,22 @@ class GameStateMachine:
         self.game_id = f"game:{generate_uuid()}"
         self.game = BotGameProcess(
             client=redis_client,
-            game_id=self.game_id
+            game_id=self.game_id,
         )
+        self.is_user_first = False
+
+    # game initialize
+    async def game_initialize(self, target: int):
+        self.is_user_first = decide_is_user_first()
+        await self.game.initializeBotPlay(
+            is_user_first=self.is_user_first,
+            target=target
+        )
+        move_info = {
+            "type": WSResponseType.MOVE_DIVISION.value,
+            "first_move": self.is_user_first
+        }
+        await self.ws_client.send_json(move_info)
 
     # State process
     async def on_enter_state(self, new_state: GameState):
