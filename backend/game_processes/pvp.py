5fb95a87a6d1b3f3d4b888091327e5bb16f5f119
NewtonZhan <zhanyunze0601@gmail.com>
2026-07-14 10:04:40 +0800

    feat(games): Add connection manager function

--- backend/game_processes/pvp.py
+++ backend/game_processes/pvp.py
@@ -1,9 +1,14 @@
-from typing import List
+from typing import List, Dict
+from logging import getLogger
+# Asyncio
 from threading import Lock
+import asyncio
 # Fastapi Dependencies
 from fastapi import WebSocket
-# Redis
-import redis.asyncio as aioredis
+# Project dependencies
+from utils.utils import generate_uuid
+
+logger = getLogger("PVP")
 
 
 # Connections
@@ -22,13 +27,22 @@ class Room:
     def __init__(
             self,
             room_id: str,
-            redis_client: aioredis.Redis,
+            public: bool,
     ):
         self.room_id: str = room_id
         self.connections: List[Connection] = []
+        self.public: bool = public
         # lock the connections list
         self.lock: Lock = Lock()
-        self.redis: aioredis.Redis = redis_client
+
+    # Delete
+    def __del__(self) -> None:
+        logger.info(f"Room {self.room_id} deleted")
+        try:
+            for connection in self.connections:
+                asyncio.run(connection.client.close())
+        except Exception as e:
+            logger.error(e)
 
     # Connect to the room
     async def connect(
@@ -45,7 +59,8 @@ class Room:
             if len(self.connections) < 2:
                 self.connections.append(new_connection)
             else:
-                raise Exception("Room fulled")
+                logger.error(f"Room {self.room_id} fulled")
+                raise Exception(f"Room {self.room_id} fulled")
 
     # Disconnect
     async def disconnect(
@@ -56,3 +71,52 @@ class Room:
             for connection in self.connections:
                 if connection.user_id == user_id:
                     self.connections.remove(connection)
+                    logger.info(f"{user_id} disconnected from room {self.room_id}")
+
+    # Broadcast
+    async def broadcast(
+            self,
+            data: dict,
+    ) -> None:
+        for connection in self.connections:
+            await connection.client.send_json(data)
+
+    # Send to certain user
+    async def send(
+            self,
+            user_id: str,
+            data: dict,
+    ) -> None:
+        for connection in self.connections:
+            if connection.user_id == user_id:
+                await connection.client.send_json(data)
+                return None
+        raise Exception(f"No such user connected to room {self.room_id}")
+
+    # Turn the data of the room to dict
+    def to_dict(self) -> dict:
+        players = []
+        for connection in self.connections:
+            players.append(connection.user_id)
+        return {
+            "room_id": self.room_id,
+            "public": self.public,
+            "player_ids": players,
+        }
+
+
+# Connection Manager
+class ConnectionManager:
+    def __init__(
+            self,
+    ):
+        self.rooms: Dict[str, Room] = {}
+
+    # Room id
+    async def new_room(self, public: bool) -> str:
+        room_id = generate_uuid()
+        self.rooms[room_id] = Room(
+            room_id=room_id,
+            public=public,
+        )
+        return room_id
